b2a2e460bfc833f4b7cd1966b856b84dcb5f363d
Paulo Costa <paulocostarn@gmail.com>
2023-01-05 15:57:25 -0300

    Foi realizada implementação da atividade

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,48 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { BASICO, MEDIO, AVANCADO }
 
+// Classe que representa um usuário
 class Usuario
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+// Classe que representa um conteúdo educacional oferecido em uma formação
+data class ConteudoEducacional(val nome: String, val duracao: Int = 60)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
-
+// Classe que representa uma formação oferecida pela DIO
+data class Formacao(val nome: String, val nivel: Nivel, var conteudos: List<ConteudoEducacional>) {
+    // Lista para armazenar os usuários matriculados na formação
     val inscritos = mutableListOf<Usuario>()
     
+    // Método para matricular usuário na formação
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    //formações com diferentes níveis
+    val formacaoBasico = Formacao("Formação Kotlin Básico", Nivel.BASICO, listOf(ConteudoEducacional("Introdução ao Kotlin")))
+    val formacaoMedio = Formacao("Formação Kotlin Médio", Nivel.MEDIO, listOf(ConteudoEducacional("Orientação a Objetos com Kotlin")))
+    val formacaoAvancado = Formacao("Formação Kotlin Avançado", Nivel.AVANCADO, listOf(ConteudoEducacional("Desenvolvimento de Aplicações Android com Kotlin")))
+
+    // usuários criados
+    val usuario1 = Usuario()
+    val usuario2 = Usuario()
+    val usuario3 = Usuario()
+
+    // Usuários matriculados nas formações
+    formacaoBasico.matricular(usuario1)
+    formacaoMedio.matricular(usuario2)
+    formacaoAvancado.matricular(usuario3)
+
+    // Cria a lista de formações
+    val formacoes = listOf(formacaoBasico, formacaoMedio, formacaoAvancado)
+
+    // Exibe a lista de formações e os alunos matriculados em cada uma delas
+    formacoes.forEach { formacao ->
+        println("Formação: ${formacao.nome} - Nível: ${formacao.nivel}")
+        println("Alunos matriculados: ${formacao.inscritos}")
+    }
 }
+
+
